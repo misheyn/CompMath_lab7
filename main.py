7b8f93de0de6bbcfe0bfaf605593bf6a97c61ccd
misheyn <am.dolmatova01@gmail.com>
2021-12-15 20:51:17 +0700

    add error value calc

--- main.py
+++ main.py
@@ -71,29 +71,41 @@ def sol_with_scipy(a, b, h):
 
 def draw_graphs(a, b, res, h, lbl):
     xi = np.arange(a, b + h, h)
+    sol = sol_with_scipy(a, b, h)
     plt.plot(xi, res, label=lbl)
-    plt.plot(sol_with_scipy(a, b, h)[0], sol_with_scipy(a, b, h)[1], label="scipy")
+    plt.plot(sol[0], sol[1], label="scipy")
     plt.scatter(xi, res, c='r')
-    plt.scatter(xi, sol_with_scipy(a, b, h)[1], c='g')
+    plt.scatter(xi, sol[1], c='g')
     plt.legend()
     plt.grid()
     plt.show()
     plt.title('Error')
-    plt.plot(xi, abs(res - sol_with_scipy(a, b, h)[1]))
+    plt.plot(xi, abs(res - sol[1]), label='Error graph ' + lbl)
+    plt.legend()
     plt.grid()
     plt.show()
+    sum = 0
+    for i in range(len(res)):
+        sum += np.std([res[i], sol[1][i]])
+    print('err = ', sum)
 
 
 x0, xn = 0, 1
 y0 = 1
 hi = 0.01
-print("euler: ", euler_method(x0, xn, y0, hi))
-print("implicit euler: ", implicit_euler_method(x0, xn, y0, hi))
-print("runge-kutta 2: ", runge_kutta2_method(x0, xn, y0, hi))
-print("runge-kutta 4: ", runge_kutta4_method(x0, xn, y0, hi))
+
+res1 = euler_method(x0, xn, y0, hi)
+res2 = implicit_euler_method(x0, xn, y0, hi)
+res3 = runge_kutta2_method(x0, xn, y0, hi)
+res4 = runge_kutta4_method(x0, xn, y0, hi)
+
+print("euler: ", res1)
+print("implicit euler: ", res2)
+print("runge-kutta 2: ", res3)
+print("runge-kutta 4: ", res4)
 print("scipy: ", sol_with_scipy(x0, xn, hi)[1])
 
-draw_graphs(x0, xn, euler_method(x0, xn, y0, hi), hi, "euler")
-draw_graphs(x0, xn, implicit_euler_method(x0, xn, y0, hi), hi, "implicit euler")
-draw_graphs(x0, xn, runge_kutta2_method(x0, xn, y0, hi), hi, "runge-kutta 2")
-draw_graphs(x0, xn, runge_kutta4_method(x0, xn, y0, hi), hi, "runge-kutta 4")
+draw_graphs(x0, xn, res1, hi, "euler")
+draw_graphs(x0, xn, res2, hi, "implicit euler")
+draw_graphs(x0, xn, res3, hi, "runge-kutta 2")
+draw_graphs(x0, xn, res4, hi, "runge-kutta 4")
